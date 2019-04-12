47093004a004e2064de2db77d3071ba536158e6a
Shrey Saxena <30777009+Shreyinfosec@users.noreply.github.com>
2019-04-12 14:30:26 +0530

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
             server: {
                 options: {
                     hostname: 'localhost',
-                    port:8080 ,
+                    port:3000,
                     livereload: true
                 }
             }
